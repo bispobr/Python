a05186e2df315fb084c99be33ae51bec0f38a038
bispobr <156684382+bispobr@users.noreply.github.com>
2024-02-12 11:16:51 -0300

    update

---
+++ EXERCICIOS/ex110/ex108.py
@@ -0,0 +1,11 @@
+import moeda
+num = int(input('digite um valor:'))
+print(f' o dobro de {moeda.moeda(num)} é {moeda.dobro(num,'s')}')
+print(f' a metade de {moeda.moeda(num)} é {(moeda.metade(num))}')
+print(f' Aumento de 10%, temos {moeda.aumentar(num,15) }')
+print(f' Diminuição de 10%, temos {moeda.diminuir(num,10)}')
+
+
+print()
+
+
