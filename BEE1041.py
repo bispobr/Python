6b6bef6cc13a22219393ae82e7df685a983e336c
bispobr <156684382+bispobr@users.noreply.github.com>
2024-03-11 09:45:28 -0300

    Create BEE1041.py

---
+++ BEE1041.py
@@ -0,0 +1,25 @@
+valores = str(input()).split()
+x = float(valores[0])
+y = float(valores[1])
+
+if (x > 0.0 ):
+    if(y > 0.0):
+        print("Q1")
+    elif(y < 0.0):
+        print("Q4")
+    else:
+        print("Eixo X")
+elif(x < 00):
+    if(y > 0.0):
+        print("Q2")
+    elif(y < 0.0):
+        print("Q3")
+    else:
+        print("Eixo X")
+else:
+    if(y > 0.0):
+        print("Eixo Y")
+    elif (y < 0.0):
+        print("Eixo Y")
+    else:
+        print("Origem")
