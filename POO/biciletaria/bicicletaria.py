0977942881b5de097fb4853782b040d2330a5588
bispobr <156684382+bispobr@users.noreply.github.com>
2024-06-24 14:43:37 -0300

    create bicicleta

---
+++ POO/biciletaria/bicicletaria.py
@@ -0,0 +1,31 @@
+class bicicleta:
+    def __init__(self,cor, modelo, ano, valor) :
+        self.cor = cor
+        self.modelo = modelo
+        self.ano= ano
+        self.valor = valor
+
+    def buzinar (self):
+        print("Bi BI")
+
+    def parar (Self):
+        print("Parando")
+
+    def correr (self):
+        print("correndo")
+
+    def __str__(self) :
+        return f"{self.__class__.__name__} : {[f"{chave} = {valor}" for chave,valor in self.__dict__.items()]}"
+
+
+b1 = bicicleta("amarela", "caloi",2010,2.500)
+
+b1.buzinar()
+b1.correr()
+b1.parar()
+
+print(b1.cor)
+print(b1.modelo)
+
+print(b1.__str__())
+
