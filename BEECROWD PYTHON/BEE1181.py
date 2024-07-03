88b68a0985e6534ef26fbe9ad1bc270258d29ee2
bispobr <156684382+bispobr@users.noreply.github.com>
2024-07-03 10:46:19 -0300

    Update BEE1181.py

--- BEECROWD PYTHON/BEE1181.py
+++ BEECROWD PYTHON/BEE1181.py
@@ -1,14 +1,20 @@
-m = [[],[],[],[],[],[],[],[],[],[],[],[]]
+m = [ [0 for i in range(12)] for j in range(12)]
+soma = 0
 
 linha = int(input())
 operacao = str(input())
 
-for coluna in range(12):
-    valor = float(input())
-    m[linha][coluna] = valor
-    soma += valor
 
-if operacao == s:
+for l in range (12):
+    for coluna in range (12):
+        valor = float(input())
+        m[l][coluna] = valor
+        if l == linha:
+            soma += valor        
+
+if operacao == "S":
     print("{:.1f}".format(soma))
-elif operacao == m:
-    print("{:.1f}".format(soma))
+elif operacao == "M":
+    print("{:.1f}".format(soma/12))
+
+
