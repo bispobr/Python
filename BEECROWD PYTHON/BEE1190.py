4558016daf96208ec9f6dfb238876cb5dc8a2bef
bispobr <156684382+bispobr@users.noreply.github.com>
2024-07-08 10:33:52 -0300

    Create BEE1190.py

---
+++ BEECROWD PYTHON/BEE1190.py
@@ -0,0 +1,17 @@
+m = [[0 for coluna in range (12)] for linha in range (12)]
+soma = 0
+
+op = str(input())
+
+for linha in range(12):
+    for coluna in range (12):
+        valor = float(input())
+        m[linha] [coluna] = valor
+
+        if (coluna + linha > 12-1) and (coluna > linha):
+            soma+=valor
+
+if op == "S":
+    print("{:.1f}".format(soma))
+elif op == "M":
+    print("{:.1f}".format(soma/12))
