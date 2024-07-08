72506c279f3411df3e70505cbe0f4b43c96a46f4
bispobr <156684382+bispobr@users.noreply.github.com>
2024-07-08 15:50:16 -0300

    Update BEE1188.py

--- BEECROWD PYTHON/BEE1188.py
+++ BEECROWD PYTHON/BEE1188.py
@@ -1,5 +1,6 @@
 m = [[0 for coluna in range(12)]for linha in range (12)]
 soma = 0
+cont = 0
 
 op = str(input())
 
@@ -9,9 +10,10 @@ for linha in range (12):
         m[linha][coluna] = valor
         if  (linha > coluna) and (coluna + linha > 12-1) :
             soma+= valor
+            cont +=1
            
 
 if op=="S":
     print ("{:.1f}".format(soma))
 elif op == "M":
-    print ("{:.1f}".format(soma/12))
+    print ("{:.1f}".format(soma/cont))
