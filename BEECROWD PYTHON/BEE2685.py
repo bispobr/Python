faacb5cbb6f9dff652a1b1f18634ffa0a26bdc56
bispobr <156684382+bispobr@users.noreply.github.com>
2024-09-17 10:33:58 -0300

    Create BEE2685.py

---
+++ BEECROWD PYTHON/BEE2685.py
@@ -0,0 +1,17 @@
+
+while True:
+    try: 
+        entrada = int(input())
+        
+        if entrada == 360:
+            print("Bom Dia!!")
+        if entrada >=0 and entrada < 90:
+            print("Bom Dia!!")
+        elif entrada >=90 and entrada < 180:
+            print("Boa Tarde!!")
+        elif entrada >=180 and entrada < 270:
+            print("Boa Noite!!")
+        elif entrada >=270 and entrada < 360:
+            print("De Madrugada!!")
+    except EOFError:
+        break
